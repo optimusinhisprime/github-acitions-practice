e55f719507cf23f8bc8bcf327d58f7f1b845609b
Agent 007 <pphiri1012x@gmail.com>
2022-12-08 22:49:51 +0200

    Add more meaningful title

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Trying Out Github Actions]</p>
+        <p>[Trying Out Github Actions now caching the Node Modules.]</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
